b9da78981b9934f159bb0a39e5d3ff6a546822fc
Thomas Decrick <thomas.decrick@icloud.com>
2023-10-09 17:50:43 +0200

    Initial Commit

---
+++ OneMinute/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OneMinute
+//
+//  Created by Thomas on 09/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OneMinute/OneMinuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneMinuteApp.swift
+//  OneMinute
+//
+//  Created by Thomas on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct OneMinuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
