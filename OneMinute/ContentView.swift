6c5ff4616d213fa20f29c020c8e034aa0bc16542
Thomas Decrick <thomas.decrick@icloud.com>
2024-09-09 00:34:07 +0200

    rewrite

--- OneMinute/ContentView.swift
+++ OneMinute/ContentView.swift
@@ -8,10 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var timer: Timer?
+    @State private var count: Int = 0
+    
     var body: some View {
-        TimerView()
+        VStack(spacing: 20) {
+            Button {
+                if timer == nil {
+                    startTimer()
+                } else {
+                    stopTimer()
+                }
+            } label: {
+                Text("\(count)")
+                    .font(.system(size: 200, weight: .bold, design: .rounded))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .cornerRadius(10.0)
+            }
+            if timer != nil {
+                Button {
+                    stopTimer()
+                } label: {
+                    Text("stop")
+                }
+            } else {
+                Text("start")
+            }
+        }
+        .foregroundStyle(.primary)
+        .padding()
+    }
+    
+    func startTimer() {
+        stopTimer()
+        count = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { timer in
+            if count < 60 {
+                count += 1
+            } else {
+                stopTimer()
+            }
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+        timer = nil
+        count = 0
     }
 }
+
 
 #Preview {
     ContentView()
